55064c9d83b3e62572b261d8675e2e7da69b3cfd
Muhammad-Huzaifah Khan <96265594+mhuzaifahkhan@users.noreply.github.com>
2022-12-08 15:49:37 +0000

    fix: correcting text in flash app

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello from Flask Docker Container.'
+    return 'Hello from Flask Docker Container...'
 
 
 if __name__ == "__main__":
